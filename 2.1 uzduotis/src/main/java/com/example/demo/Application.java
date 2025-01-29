483dfdb4f4392009eb78e49379098d8f041d82dc
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-01-29 13:41:21 +0200

    Add

---
+++ 2.1 uzduotis/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+
+	@GetMapping("/hello")
+		public String getHello(){
+		return "Hello, Spring Web!";
+		}
+}
